37feaa13bf2904d28e54d76b5eb755bd2a5c927c
Reco1l <geronimoferruccio15@gmail.com>
2023-12-22 15:53:11 -0300

    Rework Bitmap extensions

--- Toolkt/src/main/java/com/reco1l/toolkt/graphics/Bitmaps.kt
+++ Toolkt/src/main/java/com/reco1l/toolkt/graphics/Bitmaps.kt
@@ -7,54 +7,65 @@ import com.reco1l.toolkt.half
 import java.io.File
 
 
-object BitmapUtils
+// Options
+
+/**
+ * Calculates the dimensions of a image without loading a bitmap it into memory.
+ *
+ * @param file The file to get the dimensions from.
+ */
+fun Options.calculateDimensions(file: File): Options
 {
+    // This is required to be true otherwise the bitmap will be loaded into memory when we only
+    // want to know its dimensions.
+    inJustDecodeBounds = true
 
-    /**
-     * Get the dimensions of a image without loading a bitmap it into memory.
-     *
-     * @param file The file to get the dimensions from.
-     */
-    fun calculateDimensions(file: File, options: Options = Options()) = options.run {
+    // This will only decode the dimensions of the image and the MIME type due to the condition above.
+    BitmapFactory.decodeFile(file.absolutePath, this)
 
-        // This is required to be true otherwise the bitmap will be loaded into memory when we only
-        // want to know its dimensions.
-        inJustDecodeBounds = true
-
-        BitmapFactory.decodeFile(file.absolutePath, this)
-
-        // At this point we should have the dimensions of the bitmap as well the MIME type.
-        outWidth to outHeight
-    }
-
-    /**
-     * Calculates the sample size for the given dimensions.
-     *
-     * @see BitmapFactory.Options.inSampleSize
-     */
-    fun calculateSampleSize(width: Int, height: Int, targetWidth: Int, targetHeight: Int): Int
-    {
-        if (width <= targetWidth && height <= targetHeight)
-            return 1
-
-        var size = 1
-
-        while (width.half / size >= targetHeight && height.half / size >= targetHeight)
-            size *= 2
-
-        return size
-    }
-
-    /**
-     * Decodes a file into a bitmap with given dimensions.
-     */
-    fun decodeFile(file: File, targetWidth: Int, targetHeight: Int, options: Options = Options()): Bitmap?
-    {
-        val dimensions = calculateDimensions(file, options)
-
-        options.inSampleSize = calculateSampleSize(dimensions.first, dimensions.second, targetWidth, targetHeight)
-        options.inJustDecodeBounds = false
-
-        return BitmapFactory.decodeFile(file.absolutePath, options)
-    }
+    return this
 }
+
+
+/**
+ * Calculates the sample size for the current dimension, make sure to call [calculateDimensions] first.
+ *
+ * @see BitmapFactory.Options.inSampleSize
+ */
+fun Options.approximateSampleSize(targetWidth: Int, targetHeight: Int): Int
+{
+    if (outWidth <= targetWidth && outHeight <= targetHeight)
+        return 1
+
+    var size = 1
+
+    while (outWidth.half / size >= targetHeight && outHeight.half / size >= targetHeight)
+        size *= 2
+
+    inSampleSize = size
+    return size
+}
+
+
+/**
+ * Decodes a file into a bitmap with this as options.
+ */
+fun Options.createBitmap(file: File) = BitmapFactory.decodeFile(file.absolutePath, this)
+
+
+// Bitmap
+
+/**
+ * Crops the bitmap in the center to the given dimensions.
+ */
+fun Bitmap.cropInCenter(targetWidth: Int, targetHeight: Int): Bitmap
+{
+    val newX = (width - targetWidth) / 2
+    val newY = (height - targetHeight) / 2
+
+    if (newX < 0 || newY < 0)
+        return this
+
+    return Bitmap.createBitmap(this, newX, newY, targetWidth, targetHeight)
+}
+
