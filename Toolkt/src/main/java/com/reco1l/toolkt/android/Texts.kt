cf62e91d6ddeefb40b1e61e22ab1bc148a7ee89f
Reco1l <geronimoferruccio15@gmail.com>
2024-05-09 00:20:54 -0300

    Add compound drawables tools for TextViews

--- Toolkt/src/main/java/com/reco1l/toolkt/android/Texts.kt
+++ Toolkt/src/main/java/com/reco1l/toolkt/android/Texts.kt
@@ -3,6 +3,7 @@
 package com.reco1l.toolkt.android
 
 import android.graphics.Typeface
+import android.graphics.drawable.Drawable
 import android.util.TypedValue
 import android.widget.TextView
 
@@ -36,3 +37,30 @@ var TextView.fontSize: Float
 var TextView.font: Typeface
     get() = typeface
     set(value) = setTypeface(value, fontStyle)
+
+
+/// Drawables
+
+var TextView.drawableLeft: Drawable?
+    get() = compoundDrawables[0]
+    set(value) {
+        setCompoundDrawablesWithIntrinsicBounds(value, compoundDrawables[1], compoundDrawables[2], compoundDrawables[3])
+    }
+
+var TextView.drawableTop: Drawable?
+    get() = compoundDrawables[1]
+    set(value) {
+        setCompoundDrawablesWithIntrinsicBounds(compoundDrawables[0], value, compoundDrawables[2], compoundDrawables[3])
+    }
+
+var TextView.drawableRight: Drawable?
+    get() = compoundDrawables[2]
+    set(value) {
+        setCompoundDrawablesWithIntrinsicBounds(compoundDrawables[0], compoundDrawables[1], value, compoundDrawables[3])
+    }
+
+var TextView.drawableBottom: Drawable?
+    get() = compoundDrawables[3]
+    set(value) {
+        setCompoundDrawablesWithIntrinsicBounds(compoundDrawables[0], compoundDrawables[1], compoundDrawables[2], value)
+    }
