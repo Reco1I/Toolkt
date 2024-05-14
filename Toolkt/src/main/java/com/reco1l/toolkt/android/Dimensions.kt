e89747f07ea6cda835ed9efd060d608f7a3855fe
Reco1l <geronimoferruccio15@gmail.com>
2024-05-14 14:04:52 -0300

    Change dp() and sp() methods signature

--- Toolkt/src/main/java/com/reco1l/toolkt/android/Dimensions.kt
+++ Toolkt/src/main/java/com/reco1l/toolkt/android/Dimensions.kt
@@ -85,41 +85,69 @@ var View.rightPadding
 /**
  * Converts a [Float] value to pixels using [TypedValue.applyDimension].
  *
- * Warning: Using this over [Float.dp(Context)][Float.dp] is not recommended since this internally
- * uses [Resources.getSystem] as context to convert the value which may not correspond to the current
- * display metrics.
- *
  * @see COMPLEX_UNIT_DIP
  */
 val Float.dp
-    get() = TypedValue.applyDimension(COMPLEX_UNIT_DIP, this, Resources.getSystem().displayMetrics).toInt()
+    get() = TypedValue.applyDimension(COMPLEX_UNIT_DIP, this, Resources.getSystem().displayMetrics)
+
+/**
+ * Converts an [Int] value to pixels using [TypedValue.applyDimension].
+ *
+ * @see COMPLEX_UNIT_DIP
+ */
+val Int.dp
+    get() = TypedValue.applyDimension(COMPLEX_UNIT_DIP, toFloat(), Resources.getSystem().displayMetrics).toInt()
+
 
 /**
  * Converts a [Float] value to pixels using [TypedValue.applyDimension].
  *
  * @see COMPLEX_UNIT_DIP
  */
-fun Float.dp(context: Context): Int {
-    return TypedValue.applyDimension(COMPLEX_UNIT_DIP, this, context.resources.displayMetrics).toInt()
+fun Float.dp(context: Context): Float {
+    return TypedValue.applyDimension(COMPLEX_UNIT_DIP, this, context.resources.displayMetrics)
+}
+
+/**
+ * Converts an [Int] value to pixels using [TypedValue.applyDimension].
+ *
+ * @see COMPLEX_UNIT_DIP
+ */
+fun Int.dp(context: Context): Int {
+    return TypedValue.applyDimension(COMPLEX_UNIT_DIP, toFloat(), context.resources.displayMetrics).toInt()
 }
 
 /**
  * Converts a [Float] value to scaled pixels using [TypedValue.applyDimension].
  *
- * Warning: Using this over [Float.sp(Context)][Float.sp] is not recommended since this internally
- * uses [Resources.getSystem] as context to convert the value which may not correspond to the current
- * display metrics.
- *
  * @see COMPLEX_UNIT_SP
  */
 val Float.sp
-    get() = TypedValue.applyDimension(COMPLEX_UNIT_SP, this, Resources.getSystem().displayMetrics).toInt()
+    get() = TypedValue.applyDimension(COMPLEX_UNIT_SP, this, Resources.getSystem().displayMetrics)
+
+/**
+ * Converts an [Int] value to scaled pixels using [TypedValue.applyDimension].
+ *
+ * @see COMPLEX_UNIT_SP
+ */
+val Int.sp
+    get() = TypedValue.applyDimension(COMPLEX_UNIT_SP, toFloat(), Resources.getSystem().displayMetrics).toInt()
+
 
 /**
  * Converts a [Float] value to scaled pixels using [TypedValue.applyDimension].
  *
  * @see COMPLEX_UNIT_SP
  */
-fun Float.sp(context: Context): Int {
-    return TypedValue.applyDimension(COMPLEX_UNIT_SP, this, context.resources.displayMetrics).toInt()
+fun Float.sp(context: Context): Float {
+    return TypedValue.applyDimension(COMPLEX_UNIT_SP, this, context.resources.displayMetrics)
+}
+
+/**
+ * Converts an [Int] value to scaled pixels using [TypedValue.applyDimension].
+ *
+ * @see COMPLEX_UNIT_SP
+ */
+fun Int.sp(context: Context): Int {
+    return TypedValue.applyDimension(COMPLEX_UNIT_SP, toFloat(), context.resources.displayMetrics).toInt()
 }
